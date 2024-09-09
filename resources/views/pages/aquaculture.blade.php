0b4418080e3bebe48fadd8e95c999fa38fced39a
Dennis Kipchumba <kipchumbadennis10@gmail.com>
2024-09-09 17:25:07 +0300

    update lattice iimages on aquaculture

--- resources/views/pages/aquaculture.blade.php
+++ resources/views/pages/aquaculture.blade.php
@@ -296,7 +296,7 @@
                         @if ($projects && count($projects) > 0)
                             @foreach ($projects as $project)
                                 <!-- portfolio-card -->
-                                <div class="portfolio-card grid-item digital rounded-2 col-sm-6 col-md-4 col-lg-3">
+                                <div class="portfolio-card grid-item digital rounded-2 col-sm-6 col-md-4 col-lg-3 h-100">
                                     <div class="portfolio-card-body rounded-2">
                                         <div class="portfolio-card-header rounded-2">
                                             <img src="{{ Storage::url($project->image) }}" class="rounded-2"
